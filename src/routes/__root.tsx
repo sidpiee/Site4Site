66dd8ecf945265c8eb0a02aeda2c81a2a6121b48
Siddharth Sharma <sids23104@gmail.com>
2026-02-05 01:10:23 +0530

    fix: update index page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { ThemeProvider } from "@/components/Context/ThemeProvider";
-import Header from "@/components/Layout/Header";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -11,10 +10,7 @@ function RootComponent() {
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <div className="relative bg-linear-to-t from-blue-200 via-blue-300 to-blue-400 min-h-screen overflow-hidden w-full dark:bg-linear-to-t dark:from-blue-900 dark:via-indigo-900 dark:to-sky-900  ">
-          <Header />
-          <Outlet />
-        </div>
+        <Outlet />
       </ThemeProvider>
     </>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,9 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { RainbowButton } from "@/components/ui/rainbow-button.tsx";
+import { createFileRoute, Link } from "@tanstack/react-router";
+
 import { AuroraText } from "@/components/ui/aurora-text";
 import { RetroGrid } from "@/components/ui/retro-grid";
 import { TypingAnimation } from "@/components/ui/typing-animation";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
@@ -11,21 +12,23 @@ export const Route = createFileRoute("/")({
 function RouteComponent() {
   return (
     <>
-      <div className=" flex justify-center items-center h-screen flex-col p-10 relative">
-        <RetroGrid darkLineColor="white" />
+      <div className="relative bg-linear-to-t from-blue-200 via-blue-300 to-blue-400 min-h-screen  w-full dark:bg-linear-to-t dark:from-blue-950 dark:via-indigo-950 dark:to-sky-950 z-0">
+        <div className=" flex justify-center items-center min-h-screen flex-col p-10 relative">
+          <RetroGrid darkLineColor="white" />
 
-        <h1 className="font-[jost] font-bold italic text-indigo-500 dark:text-white text-6xl text-center z-10">
-          Tired of digging through <AuroraText> bookmarks </AuroraText> just to
-          find that one thing?
-        </h1>
-        <h2 className="mt-6 font-[figtree] italic  z-10">
-          <TypingAnimation typeSpeed={50} delay={1000}>
-            Save, organize, and rediscover your favorite sites with Site4Site.
-          </TypingAnimation>
-        </h2>
-        <RainbowButton variant="outline" className=" mt-5 z-10">
-          Get Started
-        </RainbowButton>
+          <h1 className="font-[jost] font-bold italic text-indigo-500 dark:text-white text-6xl text-center z-10">
+            Tired of digging through <AuroraText> bookmarks </AuroraText> just
+            to find that one thing?
+          </h1>
+          <h2 className="mt-6 font-[figtree] italic  z-10">
+            <TypingAnimation typeSpeed={30} delay={1000}>
+              Save, organize, and rediscover your favorite sites with Site4Site.
+            </TypingAnimation>
+          </h2>
+          <Link to="/docs">
+            <Button className=" relative z-50">Get Started</Button>
+          </Link>
+        </div>
       </div>
     </>
   );
