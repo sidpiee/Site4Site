0553e22c17d6ceeb19490e182b56d0d80b2638fe
Siddharth Sharma <sids23104@gmail.com>
2026-02-21 21:55:37 +0530

    feat: add searchbar to anime-page

--- src/routes/anime/index.tsx
+++ src/routes/anime/index.tsx
@@ -1,5 +1,6 @@
 import MainLayout from "@/components/Layout/MainLayout";
 import AnimeCard from "@/components/ui/anime-card";
+import { SearchBar } from "@/components/ui/searchbar";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/anime/")({
@@ -9,6 +10,9 @@ export const Route = createFileRoute("/anime/")({
 function RouteComponent() {
   return (
     <MainLayout>
+      <div className="mb-10">
+        <SearchBar placeholder="Death Note..." />
+      </div>
       <AnimeCard />
     </MainLayout>
   );
