8aa2821a84b71b898bb7424ba12eded5e6f8b66c
Siddharth Sharma <sids23104@gmail.com>
2026-03-01 23:43:47 +0530

    feat: add sections on anime page

--- src/components/ui/loding-state.tsx
+++ src/components/ui/loding-state.tsx
@@ -3,7 +3,7 @@ import { Spinner } from "./spinner";
 export default function Loding() {
   return (
     <>
-      <div className="max-w-sm  bg-slate-100 dark:bg-card border-black/30 dark:border-white/40 border gap-5 flex justify-start items-center h-26 mt-2 overflow-hidden rounded-l-sm">
+      <div className="max-w-sm  bg-slate-100 dark:bg-card border-black/30 dark:border-white/40 border gap-5 flex justify-start items-center h-26 overflow-hidden rounded-l-sm">
         <Spinner className="size-6" />
         <p className="font-[Figtree] text-lg">Loding...</p>
       </div>
--- src/routes/anime/index.tsx
+++ src/routes/anime/index.tsx
@@ -2,7 +2,7 @@ import MainLayout from "@/components/Layout/MainLayout";
 import AnimeCard from "@/components/ui/anime-card";
 import { SearchBar } from "@/components/ui/searchbar";
 import { createFileRoute } from "@tanstack/react-router";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import useDebounce from "@/hooks/useDebounce";
 import { useQuery } from "@tanstack/react-query";
 import SearchResult from "@/components/ui/search-result";
@@ -40,14 +40,18 @@ function RouteComponent() {
           Search={search}
           SetSearch={setSearch}
         />
-        {isLoading && <Loding />}
+        {isLoading && (
+          <div className="absolute top-full left-0 shadow-lg  mt-2 z-20 min-w-sm">
+            <Loding />
+          </div>
+        )}
         {isError && (
           <p className="mt-5 text-red-500 font-semibold font-[Figtree] ">
             {error.message}
           </p>
         )}
         {data?.data?.length > 0 && (
-          <div className="absolute top-full left-0 shadow-lg  mt-2 z-20 min-w-sm max-h-100 overflow-y-auto no-scrollbar">
+          <div className="absolute top-full left-0 shadow-lg  mt-2 z-20 min-w-sm max-h-90 overflow-y-auto no-scrollbar">
             {data.data.map((anime) => (
               <SearchResult
                 key={anime.mal_id}
@@ -58,7 +62,39 @@ function RouteComponent() {
           </div>
         )}
       </div>
-      <AnimeCard />
+      <div className="mb-10">
+        <section>
+          <h1 className="text-6xl  text-transparent bg-linear-to-r from-violet-500 to-violet-700 font-bold inline-block bg-clip-text font-[Urbanist] italic">
+            Plan to Watch
+          </h1>
+          <div className="h-1 w-110 bg-linear-to-r from-purple-500 to-purple-800 mt-3 mb-8 rounded-full" />
+          <div className="grid grid-cols-4 gap-x-2 gap-y-10">
+            <AnimeCard />
+          </div>
+        </section>
+      </div>
+      <div className="mb-10">
+        <section>
+          <h1 className="text-6xl  text-transparent bg-linear-to-r from-indigo-500 to-indigo-700 font-bold inline-block bg-clip-text font-[Urbanist] italic">
+            Watching
+          </h1>
+          <div className="h-1 w-140 bg-linear-to-r from-blue-500 to-blue-800 mt-3 mb-8 rounded-full" />
+          <div className="grid grid-cols-4 gap-x-2 gap-y-10">
+            <AnimeCard />
+          </div>
+        </section>
+      </div>
+      <div className="mb-10">
+        <section>
+          <h1 className="text-6xl  text-transparent bg-linear-to-r from-emerald-500 to-emerald-700 font-bold inline-block bg-clip-text font-[Urbanist] italic">
+            Completed
+          </h1>
+          <div className="h-1 w-180 bg-linear-to-r from-green-500 to-green-800 mt-3 mb-8 rounded-full" />
+          <div className="grid grid-cols-4 gap-x-2 gap-y-10">
+            <AnimeCard />
+          </div>
+        </section>
+      </div>
     </MainLayout>
   );
 }
