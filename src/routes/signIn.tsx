ca549aed876a29807b0e4e1b37b7f2eb0fe79aa8
sidpiee <sids23104@gmail.com>
2026-07-24 02:35:19 +0530

    fix: dark mode for signin page

--- src/routes/signIn.tsx
+++ src/routes/signIn.tsx
@@ -67,57 +67,59 @@ function RouteComponent() {
   }
 
   return (
-    <div className=" bg-linear-to-tr from-black via-white/5 to-black h-screen flex flex-col items-center gap-10">
-      <img
-        src={logo}
-        alt=""
-        className="z-0 opacity-5 h-screen w-full absolute"
-      />
-      <h1 className="text-5xl font-extrabold italic tracking-tight font-[Zalando_Sans_Expanded] text-center pt-10 z-10 ">
-        <Link to="/" className="">
-          <span className="text-indigo-500">SITE</span>
-          <span className="">4</span>
-          <span className="text-indigo-500">SITE</span>
-        </Link>
-      </h1>
-      <div className="border-2 border-white/30  backdrop-blur-4xl bg-black/50  p-5 w-100 rounded-lg z-10 ">
-        <p className="text-center text-3xl mb-5 font-bold font-[Urbanist]">
-          Verify Account
-        </p>
-        <FieldSet className="w-full max-w-xs flex flex-col ">
-          <FieldGroup>
-            <Field>
-              <FieldLabel htmlFor="Email">Email</FieldLabel>
-              <Input
-                id="Email"
-                type="email"
-                placeholder="123@a.com"
-                value={email}
-                onChange={(e) => {
-                  setSent(false);
-                  setEmail(e.target.value);
-                }}
-              />
-            </Field>
-          </FieldGroup>
+    <div className="dark">
+      <div className="min-h-screen bg-background text-foreground bg-linear-to-tr from-black via-white/5 to-black flex flex-col items-center gap-10">
+        <img
+          src={logo}
+          alt=""
+          className="z-0 opacity-5 h-screen w-full absolute"
+        />
+        <h1 className="text-5xl font-extrabold italic tracking-tight font-[Zalando_Sans_Expanded] text-center pt-10 z-10 ">
+          <Link to="/" className="">
+            <span className="text-indigo-500">SITE</span>
+            <span className="">4</span>
+            <span className="text-indigo-500">SITE</span>
+          </Link>
+        </h1>
+        <div className="border-2 border-white/30  backdrop-blur-4xl bg-black/50  p-5 w-100 rounded-lg z-10 ">
+          <p className="text-center text-3xl mb-5 font-bold font-[Urbanist]">
+            Verify Account
+          </p>
+          <FieldSet className="w-full max-w-xs flex flex-col ">
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="Email">Email</FieldLabel>
+                <Input
+                  id="Email"
+                  type="email"
+                  placeholder="123@a.com"
+                  value={email}
+                  onChange={(e) => {
+                    setSent(false);
+                    setEmail(e.target.value);
+                  }}
+                />
+              </Field>
+            </FieldGroup>
 
-          <Button
-            onClick={sendMail}
-            disabled={cooldown > 0 || loding}
-            className="cursor-pointer bg-black drop-shadow-sm drop-shadow-white/20 hover:bg-slate-800 hover:border-blue-600 border-2 transition-all duration-150"
-          >
-            {loding
-              ? 'Loading...'
-              : cooldown > 0
-                ? `Resend Link in ${cooldown}s`
-                : 'Send Link'}
-          </Button>
-          <FieldDescription className="text-center">
-            {!sent
-              ? 'A link will be sent on this email !'
-              : `Link sent on email ${email}`}
-          </FieldDescription>
-        </FieldSet>
+            <Button
+              onClick={sendMail}
+              disabled={cooldown > 0 || loding}
+              className="cursor-pointer bg-black drop-shadow-sm drop-shadow-white/20 hover:bg-slate-800 hover:border-blue-600 border-2 transition-all duration-150"
+            >
+              {loding
+                ? 'Loading...'
+                : cooldown > 0
+                  ? `Resend Link in ${cooldown}s`
+                  : 'Send Link'}
+            </Button>
+            <FieldDescription className="text-center">
+              {!sent
+                ? 'A link will be sent on this email !'
+                : `Link sent on email ${email}`}
+            </FieldDescription>
+          </FieldSet>
+        </div>
       </div>
     </div>
   );
